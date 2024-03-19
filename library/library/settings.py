d0406d5f2617f7e002fd7b596d7fb7bcc2aa22f2
faizan2700 <syedfaizan824@gmail.com>
2024-03-19 10:38:12 +0530

    updated documentation

--- library/library/settings.py
+++ library/library/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k(rdpeyp%$_!6-))6&*9x4y-rxx$!z&b5rxuhzlfessb0x)e%f'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
+
     # 3rd party apps 
     'rest_framework', 
-    'django_filters', 
-    'django_extensions', 
+    'django_filters',  
+    'drf_yasg',   
 
     # dev apps 
     'main', 
@@ -83,11 +85,11 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library_db', 
-        'HOST': 'localhost', 
-        'PORT': '5432', 
-        'USER': 'postgres', 
-        'PASSWORD': 'admin', 
+        'NAME': config('DB_NAME'), 
+        'HOST': config('DB_HOST'), 
+        'PORT': config('DB_PORT'), 
+        'USER': config('DB_USER'), 
+        'PASSWORD': config('DB_PASSWORD'), 
     }
 }
 
