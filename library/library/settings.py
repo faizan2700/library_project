9082a1f35e59d436cf155862aa9f8e0e45e06649
faizan2700 <syedfaizan824@gmail.com>
2024-03-20 17:52:18 +0530

    updated github actions

--- library/library/settings.py
+++ library/library/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path 
-from decouple import config 
+from dotenv import load_dotenv  
+import os 
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', 'secret_key') 
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,11 +94,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'library_db5', 
-        'HOST': config('DB_HOST', 'localhost'), 
-        'PORT': config('DB_PORT', '5432'), 
-        'USER': config('DB_USER', 'postgres'), 
-        'PASSWORD': config('DB_PASSWORD', 'admin'), 
+        'NAME': os.getenv('DB_NAME'), 
+        'HOST': os.getenv('DB_HOST', 'localhost'), 
+        'PORT': os.getenv('DB_PORT', '5432'), 
+        'USER': os.getenv('DB_USER', 'postgres'), 
+        'PASSWORD': os.getenv('DB_PASSWORD', 'admin'), 
     }
 }
 
