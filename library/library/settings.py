bb43f72b3bea9f84ca0a431ba260227004a9fcae
faizan2700 <syedfaizan824@gmail.com>
2024-03-20 15:03:45 +0530

    updated database settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = config('SECRET_KEY', 'secret_key') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,12 +89,13 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library_db5', 
-        'HOST': config('DB_HOST'), 
-        'PORT': config('DB_PORT'), 
-        'USER': config('DB_USER'), 
-        'PASSWORD': config('DB_PASSWORD'), 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': 'library_db5', 
+        # 'HOST': config('DB_HOST'), 
+        # 'PORT': config('DB_PORT'), 
+        # 'USER': config('DB_USER'), 
+        # 'PASSWORD': config('DB_PASSWORD'), 
     }
 }
 
