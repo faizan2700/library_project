46df4987ee1cede27cd3bc0504aec3236888590a
faizan2700 <syedfaizan824@gmail.com>
2024-04-17 00:41:44 +0530

    updated docker compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Run Django tests
-python manage.py test
+# Run Django tests 
+python library/manage.py makemigrations 
+python library/manage.py migrate 
+python library/manage.py test
 
 # Check the exit code of the test command
 if [ $? -eq 0 ]; then
   # If tests pass, start the server
-  python manage.py runserver 0.0.0.0:8000
+  python library/manage.py runserver 0.0.0.0:8000
 else
   # If tests fail, exit with a non-zero status code
   echo "Tests failed. Exiting..."
